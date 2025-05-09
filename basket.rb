04575582ab6fd679e27c931ca08dc4a5fc636801
sammyremi <adebayorsamuelas@gmail.com>
2025-05-09 15:00:15 +0100

    add examples for baskets

--- basket.rb
+++ basket.rb
@@ -2,6 +2,7 @@ class Basket
     def initialize(products:, delivery_rules:, offers: [])
       @products = products
       @delivery_rules = delivery_rules
+      @offers = offers
       @items = []
     end
   
@@ -35,7 +36,7 @@ class Basket
           return rule[:charge]
         end
       end
-      return 0.0
+      0.0
     end
   
     def apply_red_widget_offer
@@ -48,5 +49,44 @@ class Basket
       discount_per_item = @products["R01"][:price] / 2
       half_price_items * discount_per_item
     end
-end
+  end
+  
+  # Product catalogue
+  PRODUCTS = {
+    "R01" => { name: "Red Widget", price: 32.95 },
+    "G01" => { name: "Green Widget", price: 24.95 },
+    "B01" => { name: "Blue Widget", price: 7.95 }
+  }
+  
+  # Delivery rules
+  DELIVERY_RULES = [
+    { threshold: 50.0, charge: 4.95 },
+    { threshold: 90.0, charge: 2.95 }
+  ]
+  
+  # Example usage
+  puts "Example Baskets:"
+  
+  basket1 = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES)
+  basket1.add("B01")
+  basket1.add("G01")
+  puts "B01, G01 => $#{basket1.total}"
+  
+  basket2 = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES)
+  basket2.add("R01")
+  basket2.add("R01")
+  puts "R01, R01 => $#{basket2.total}"
+  
+  basket3 = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES)
+  basket3.add("R01")
+  basket3.add("G01")
+  puts "R01, G01 => $#{basket3.total}" 
+  
+  basket4 = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES)
+  basket4.add("B01")
+  basket4.add("B01")
+  basket4.add("R01")
+  basket4.add("R01")
+  basket4.add("R01")
+  puts "B01, B01, R01, R01, R01 => $#{basket4.total}" 
   
