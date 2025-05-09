826e587bef0fbe5f8ff3e8003056c6b8a348730e
sammyremi <adebayorsamuelas@gmail.com>
2025-05-09 09:53:40 +0100

    Implemented  method to append valid product codes to @items array | calculate final basket total

--- basket.rb
+++ basket.rb
@@ -5,4 +5,20 @@ class Basket
         @offers = offers
         @items = []
     end
+
+    def add(product_code)
+        raise "Invalid product code" unless @products.key?(product_code)
+    
+        @items << product_code
+    end
+    
+    def total
+        subtotal = calculate_subtotal
+        discount = apply_offers
+        delivery_fee = calculate_delivery_fee(subtotal - discount)
+    
+        final_amount = subtotal - discount + delivery_fee
+        format('%.2f', final_amount).to_f
+    end
+    
 end
